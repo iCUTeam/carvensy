3956c8494e47bd4b161bd3c9cc6d7d73b7e011ef
Kathleen <kathy.febiola02@gmail.com>
2022-06-14 19:01:37 +0700

    Test

---
+++ Break_Plan+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Break_Plan+CoreDataClass.swift
+//  carvensy
+//
+//  Created by Kathleen Febiola Susanto on 14/06/22.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class Break_Plan: NSManagedObject {
+
+}
---
+++ Stretch+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Stretch+CoreDataClass.swift
+//  carvensy
+//
+//  Created by Kathleen Febiola Susanto on 14/06/22.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class Stretch: NSManagedObject {
+
+}
