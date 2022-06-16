586483795c36ff9771107c36ca96489bfd1848c4
kego <42563702+kegoaps@users.noreply.github.com>
2022-06-16 14:25:04 +0700

    change main and viewcontroller with timerpage and timerpagecontroller

--- carvensy/Features/Breaktimer/Pages/Break/Controller/BreakPageController.swift
+++ carvensy/Features/Breaktimer/Pages/Break/Controller/BreakPageController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class BreakPageController: ViewController {
+class BreakPageController: TimerPageController {
     @IBOutlet weak var image: UIImageView!
     @IBOutlet weak var stretchButton: UIButton!
     @IBOutlet weak var finishButton: UIButton!
--- carvensy/Features/Breaktimer/Pages/Timer/Controller/TimerPageController.swift
+++ carvensy/Features/Breaktimer/Pages/Timer/Controller/TimerPageController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TimerPageController: UIViewController {
 
     @IBOutlet weak var timerLabel: UILabel!
     
