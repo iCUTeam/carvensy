b2eb685a6b63492be045a97856fbbd8b0c2fc221
kego <42563702+kegoaps@users.noreply.github.com>
2022-06-16 13:38:09 +0700

    launch screen and colors

--- carvensy/Features/Breaktimer/Pages/Timer/Controller/ViewController.swift
+++ carvensy/Features/Breaktimer/Pages/Timer/Controller/ViewController.swift
@@ -9,9 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timerLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        timerLabel.textColor = CarvensyColor.greenMain
     }
 
 
