6d2ae07c2a58aa59ab1cacbc4a5b987b44e61019
kego <42563702+kegoaps@users.noreply.github.com>
2022-06-16 14:37:23 +0700

    fix review

--- carvensy/Features/Breaktimer/Pages/Break/Controller/BreakPageController.swift
+++ carvensy/Features/Breaktimer/Pages/Break/Controller/BreakPageController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class BreakPageController: TimerPageController {
+class BreakPageController: UIViewController {
     @IBOutlet weak var image: UIImageView!
     @IBOutlet weak var stretchButton: UIButton!
     @IBOutlet weak var finishButton: UIButton!
