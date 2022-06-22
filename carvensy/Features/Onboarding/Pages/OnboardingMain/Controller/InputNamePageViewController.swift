d9937a107cf79b3cdfed69dc59fdcccbe23e379d
Ariel Waraney Manueke <arielwaraneymanueke01@gmail.com>
2022-06-22 19:31:47 +0900

    Adding Asset + Changing Main Green Color + Onboarding Main Flow (Intro -> input name with keyboard ui adjustment -> pain assessment page card clickable UI.

--- carvensy/Features/Onboarding/Pages/OnboardingMain/Controller/InputNamePageViewController.swift
+++ carvensy/Features/Onboarding/Pages/OnboardingMain/Controller/InputNamePageViewController.swift
@@ -9,20 +9,76 @@ import UIKit
 
 class InputNamePageViewController: UIViewController {
     @IBOutlet weak var userName: UITextField!
+    @IBOutlet weak var nameCardView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        nameCardView.roundViewCorner([.topLeft,.topRight,.bottomLeft,.bottomRight], radius: 10)
         
         //hide the selector when tap outside
         self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(hideSelector)))
         //dismiss keyboard when return key pressed
         userName.delegate = self
+        
+        //keyboard appearance
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardHide), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
 
     @objc private func hideSelector(){
         self.view.endEditing(true)
     }
     
+    @objc private func keyboardShow(notification: NSNotification){
+        guard let userInfo = notification.userInfo,
+              let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue,
+              let currentTextField = UIResponder.currentFirst() as? UITextField else { return }
+        
+        //check if the top of the keybaord is above the bottom of the current focused textbox
+        let keyboardTopY = keyboardFrame.cgRectValue.origin.y
+        let convertedTextFieldFrame = view.convert(currentTextField.frame, from: currentTextField.superview)
+        let textFieldBottomY = convertedTextFieldFrame.origin.y + convertedTextFieldFrame.size.height
+        
+        //make sure the swipe only once above the default position
+        if view.frame.origin.y >= 0 {
+            //check if text field bottom is below keybaord bottom - bump the frame up
+            if textFieldBottomY > keyboardTopY {
+                let textBoxY = convertedTextFieldFrame.origin.y
+                let newFrameY = ((textBoxY - keyboardTopY / 2) * -1) + 50
+                view.frame.origin.y = newFrameY
+            }
+        }
+    }
+    
+    @objc private func keyboardHide(){
+        self.view.frame.origin.y = 0
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
     //TODO: Push view upper for text field visibility when keyboard toggled on.
+}
+
+extension UIResponder {
+    
+    private struct Static {
+        static weak var responder: UIResponder?
+    }
+    
+    //find the current first responder and return the current Responder if exsist
+    
+    static func currentFirst()-> UIResponder? {
+        Static.responder = nil
+        UIApplication.shared.sendAction(#selector(UIResponder._trap), to: nil, from: nil, for: nil)
+        return Static.responder
+    }
+    
+    @objc private func _trap(){
+        //the element selected
+        Static.responder = self
+    }
 }
--- carvensy/Features/Onboarding/Pages/OnboardingMain/Controller/IntroductionPageViewController.swift
+++ carvensy/Features/Onboarding/Pages/OnboardingMain/Controller/IntroductionPageViewController.swift
@@ -9,11 +9,14 @@ import UIKit
 
 class IntroductionPageViewController: UIViewController {
     @IBOutlet weak var aboutButton: UIButton!
+    @IBOutlet weak var breakCardView: UIView!
+    @IBOutlet weak var stretchCardView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        breakCardView.roundViewCorner([.bottomRight,.bottomLeft,.topRight,.topLeft], radius: 10)
+        stretchCardView.roundViewCorner([.bottomRight,.bottomLeft,.topRight,.topLeft], radius: 10)
     }
     
     @IBAction func aboutButtonPressed(_ sender: Any) {
@@ -22,4 +25,32 @@ class IntroductionPageViewController: UIViewController {
     }
     
 
+}
+
+extension UIView {
+    
+    func roundViewCorner(_ corners: UIRectCorner, radius: CGFloat){
+        if #available(iOS 11, *){
+            var cornerMask = CACornerMask()
+            if(corners.contains(.topLeft)){
+                cornerMask.insert(.layerMinXMinYCorner)
+            }
+            if(corners.contains(.topRight)){
+                cornerMask.insert(.layerMaxXMinYCorner)
+            }
+            if(corners.contains(.bottomLeft)){
+                cornerMask.insert(.layerMinXMaxYCorner)
+            }
+            if(corners.contains(.bottomRight)){
+                cornerMask.insert(.layerMaxXMaxYCorner)
+            }
+            self.layer.cornerRadius = radius
+            self.layer.maskedCorners = cornerMask
+        } else {
+            let path = UIBezierPath(roundedRect: self.bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+            let mask = CAShapeLayer()
+            mask.path = path.cgPath
+            self.layer.mask = mask
+        }
+    }
 }
