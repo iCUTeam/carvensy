a128739e979b5a0ac0970d0e7049d83711b474af
Kathleen <kathy.febiola02@gmail.com>
2022-06-22 21:06:37 +0700

    Fix Slow GIF

--- carvensy/Features/Stretch/Pages/ChooseStretch/Controller/ChooseStretchController.swift
+++ carvensy/Features/Stretch/Pages/ChooseStretch/Controller/ChooseStretchController.swift
@@ -26,19 +26,19 @@ class ChooseStretchController: UIViewController, UITableViewDataSource, UITableV
         self.tableView.register(UINib(nibName: "ChooseStretchTableViewCell", bundle: nil), forCellReuseIdentifier: "stretch-type")
         self.tableView.register(UINib(nibName: "MoreComingSoonCell", bundle: nil), forCellReuseIdentifier: "more")
         
-        quickStretchSteps.append(StretchSteps(title: "Push Out", desc: "Interlace your fingers as shown on the guide. Then rotate your palms and push them out and away from your chest.", icon: "Push Out Stretch", reps: 2, holdSec: 5, bothHand: false))
+        quickStretchSteps.append(StretchSteps(title: "Push Out", desc: "Interlace your fingers as shown on the guide. Then rotate your palms and push them out and away from your chest.", icon: "Push Out", reps: 2, holdSec: 5, bothHand: false))
         quickStretchSteps.append(StretchSteps(title: "Stop - Wrist Extension", desc: "Start by holding one hand up and extended all the way out. It’s as if you’re saying, “Stop!” With the other hand, reach out and pull your fingers back.", icon: "Stop Stretch", reps: 2, holdSec: 5, bothHand: true))
-        quickStretchSteps.append(StretchSteps(title: "Prayer", desc: "At chest level, press both palms together as on the given guide, keeping your hands at the same level, slowly raise both elbows.", icon: "Prayer", reps: 2, holdSec: 5, bothHand: false))
-        quickStretchSteps.append(StretchSteps(title: "Thumb Glide", desc: "As in the guide, start by grabbing your thumb. Then rotate it like a helicopter blade. When finished, gently pull your thumb backward.", icon: "Thumb Glide", reps: 2, holdSec: 5, bothHand: true))
+        quickStretchSteps.append(StretchSteps(title: "Prayer Pose", desc: "At chest level, press both palms together as on the given guide, keeping your hands at the same level, slowly raise both elbows.", icon: "Prayer", reps: 2, holdSec: 5, bothHand: false))
+        quickStretchSteps.append(StretchSteps(title: "Thumb Glide", desc: "As in the guide, start by grabbing your thumb. Then rotate it like a helicopter blade. When finished, gently pull your thumb backward.", icon: "Thumb Glides", reps: 2, holdSec: 5, bothHand: true))
         
-        focusStretchSteps.append(StretchSteps(title: "Push Out", desc: "Interlace your fingers as shown on the guide. Then rotate your palms and push them out and away from your chest.", icon: "Push Out Stretch", reps: 2, holdSec: 10, bothHand: false))
+        focusStretchSteps.append(StretchSteps(title: "Push Out", desc: "Interlace your fingers as shown on the guide. Then rotate your palms and push them out and away from your chest.", icon: "Push Out", reps: 2, holdSec: 10, bothHand: false))
         focusStretchSteps.append(StretchSteps(title: "Stop - Wrist Extension", desc: "Start by holding one hand up and extended all the way out. It’s as if you’re saying, “Stop!” With the other hand, reach out and pull your fingers back.", icon: "Stop Stretch", reps: 2, holdSec: 10, bothHand: true))
-        focusStretchSteps.append(StretchSteps(title: "Prayer", desc: "At chest level, press both palms together as on the given guide, keeping your hands at the same level, slowly raise both elbows.", icon: "Prayer", reps: 2, holdSec: 10, bothHand: false))
-        focusStretchSteps.append(StretchSteps(title: "Thumb Glide", desc: "As in the guide, start by grabbing your thumb. Then rotate it like a helicopter blade. When finished, gently pull your thumb backward.", icon: "Thumb Glide", reps: 2, holdSec: 10, bothHand: true))
+        focusStretchSteps.append(StretchSteps(title: "Prayer Pose", desc: "At chest level, press both palms together as on the given guide, keeping your hands at the same level, slowly raise both elbows.", icon: "Prayer", reps: 2, holdSec: 10, bothHand: false))
+        focusStretchSteps.append(StretchSteps(title: "Thumb Glide", desc: "As in the guide, start by grabbing your thumb. Then rotate it like a helicopter blade. When finished, gently pull your thumb backward.", icon: "Thumb Glides", reps: 2, holdSec: 10, bothHand: true))
         
         
         stretchType.append(StretchType(title: "Quick Stretch (1 mins)", content: "Static - 4 Stretch Moves", icon: "Stop Stretch", steps: quickStretchSteps))
-        stretchType.append(StretchType(title: "Focus Stretch (2 mins)", content: "Static - 4 Stretch Moves", icon: "Push Out Stretch", steps: focusStretchSteps))
+        stretchType.append(StretchType(title: "Focus Stretch (2 mins)", content: "Static - 4 Stretch Moves", icon: "Push Out", steps: focusStretchSteps))
         
         
 
